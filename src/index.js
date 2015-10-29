86908c0a3ae33486a46ad8fcaef03f5f6c16fc98
Oleg Elifantiev <oleg@elifantiev.ru>
2015-10-30 01:03:05 +0300

    Debug. Adding a devtools and log monitor

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers/index.js';
 import middleware from './middlewares/serverMiddleware.js';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import DiffMonitor from 'redux-devtools-diff-monitor';
 
 var initialState = {
    userName: null,
@@ -15,13 +18,20 @@ var initialState = {
 };
 
 var createStoreWithMiddlewares = compose(
-   applyMiddleware(middleware)
+   applyMiddleware(middleware),
+   devTools(),
+   persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
 
 var store = createStoreWithMiddlewares(reducer, initialState);
 
 ReactDOM.render(
-   <Provider store={store}>
-      <App />
-   </Provider>,
+   <div>
+      <Provider store={store}>
+         <App />
+      </Provider>
+      <DebugPanel top right bottom>
+         <DevTools store={store} monitor={SliderMonitor} />
+      </DebugPanel>
+   </div>,
    document.getElementById('root'));
